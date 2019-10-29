2cad0ba5bb4f44a65af798ac76e3a80c6337af86
Schmucus <m.haeseler@freenet.de>
2019-10-29 22:07:44 +0100

    2 files modified

--- src/app/logs/logs.component.ts
+++ src/app/logs/logs.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Log } from '../log';
 
 @Component({
   selector: 'app-logs',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogsComponent implements OnInit {
 
-  @Input()
+  @Input() log: Log[];
 
   constructor() { }
 
